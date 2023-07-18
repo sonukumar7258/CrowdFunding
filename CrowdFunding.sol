4763a405fc6617510ce56b1861f15b707e8e84d7
Sonu Kumar <68735161+sonukumar7258@users.noreply.github.com>
2023-07-18 13:05:29 +0500

    Update CrowdFunding.sol
    Add Description for each functionality

--- CrowdFunding.sol
+++ CrowdFunding.sol
@@ -3,65 +3,58 @@ pragma solidity ^0.8.0;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
-
-contract CrowdFunding{
+/**
+ * @title CrowdFunding
+ * @dev This contract allows crowdfunding using Ether (ETH) with a minimum required funding amount in USD equivalent.
+ * The manager deploys the contract and can receive the collected funds once the campaign is over.
+ */
+contract CrowdFunding {
     using PriceConverter for uint256;
-    uint256 public mininumUsd = 5e18;
-    address manager;
-    address[] funders;
-    mapping(address => uint256) AddresstoUsdAmount;
 
-    constructor(){
+    uint256 public mininumUsd = 5e18; // Minimum amount in USD required to participate in the crowdfunding campaign.
+    address public manager; // Address of the manager who deploys and controls the crowdfunding campaign.
+    address[] public funders; // Addresses of participants who funded the campaign.
+    mapping(address => uint256) public AddresstoUsdAmount; // Mapping to track the USD amount funded by each participant.
+
+    /**
+     * @dev Constructor function sets the manager's address as the deployer of the contract.
+     */
+    constructor() {
         manager = msg.sender;
     }
 
-    modifier onlyOwner(){
+    /**
+     * @dev Modifier to restrict access to only the manager of the crowdfunding campaign.
+     */
+    modifier onlyOwner() {
         require(msg.sender == manager, "Only manager can perform this action");
         _;
     }
 
+    /**
+     * @dev Allows anyone to fund the crowdfunding campaign by sending ETH with a value equivalent to at least the minimum USD amount.
+     */
     function fund() public payable {
-        require(msg.value.getConversionRate() >= mininumUsd, "You have to fund atleast 5usd!");
-        // becasue anyone can fund
+        require(msg.value.getConversionRate() >= mininumUsd, "You have to fund at least 5 USD!");
+        // Anyone can fund the campaign.
         funders.push(msg.sender);
         AddresstoUsdAmount[msg.sender] += msg.value;
     }
 
-    function withdraw() public onlyOwner{
-
-        for(uint i = 0; i < funders.length; i++){
+    /**
+     * @dev Allows the manager to withdraw the collected funds after the campaign is over.
+     * It resets the funders' balances and removes their addresses from the list.
+     */
+    function withdraw() public onlyOwner {
+        for (uint256 i = 0; i < funders.length; i++) {
             address funder = funders[i];
             AddresstoUsdAmount[funder] = 0;
-        } 
+        }
         funders = new address[](0);
 
-        // payable(msg.sender).transfer(address(this).balance);
-
-        (bool success,) = payable(msg.sender).call{value: address(this).balance}("");
-        require(success,"Call Failed!");
+        // Transfer the contract balance to the manager's address.
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Call Failed!");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
